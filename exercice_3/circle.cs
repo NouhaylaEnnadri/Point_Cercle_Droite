373c77e302c4fe8ff68ca6ed95212810c63e6180
Nouhayla <nouhayla.ennadri@usmba.ac.ma>
2022-11-05 20:57:25 +0100

    Add project files.

---
+++ exercice_3/circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_3
+{
+    internal class circle
+    {
+        class Cercle
+        {
+            private point centre;
+            private int rayon;
+
+            public int GetRayon()
+            {
+                return rayon;
+            }
+            public point GetCentre()
+            {
+                return centre;
+            }
+            public void SetRayon(int rayon)
+            {
+                this.rayon = rayon;
+            }
+            public void SetCenter(point centre)
+            {
+                this.centre = centre;
+            }
+            public Cercle(point centre, int rayon = 0)
+            {
+                centre = new point(0, 0);
+                this.centre = centre;
+                this.rayon = rayon;
+            }
+        }
+    }
+}
---
+++ exercice_3/point.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_3
+{
+    internal class point
+    {
+        private int x; 
+        private int y;
+
+        public point (int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int Getx()
+        {
+            return x;   
+        }
+        public int Gety()
+        {
+            return y;
+        }
+        public void Setx(int x )
+        {
+            this.x = x; 
+        }
+        public void Sety( int y)
+        {
+            this.y = y;
+        }
+        
+        public point Transaltion(point q) {
+         point p_resulat = new point (x+q.x , y+q.y);
+            return p_resulat;
+           
+
+        }
+
+      public int Distance(point p , point q)
+        {
+
+            int resu; 
+            q = new point (x,y);
+            p= new point(x, y);
+            resu = (int)Math.Sqrt( Math.Pow(p.x-q.x,2) - Math.Pow(p.y-q.y,2) ); 
+            return resu; 
+
+        }
+    }
+}
