d4fe2c45da72aa3aab3bb4c5072cbb683e5a910b
Nouhayla <nouhayla.ennadri@usmba.ac.ma>
2022-11-21 17:09:03 +0100

    modification de classe point

--- exercice_3/point.cs
+++ exercice_3/point.cs
@@ -8,48 +8,54 @@ namespace exercice_3
 {
     internal class point
     {
-        private int x; 
-        private int y;
+        //declaration des variables x,y dans le plan 
+        public int x { get; set; }
+        private int y { get; set; }
 
-        public point (int x, int y)
+
+        //initialisation des attributs 
+        public point(int x, int y)
         {
             this.x = x;
             this.y = y;
         }
-        public int Getx()
-        {
-            return x;   
-        }
-        public int Gety()
-        {
-            return y;
-        }
-        public void Setx(int x )
-        {
-            this.x = x; 
-        }
-        public void Sety( int y)
-        {
-            this.y = y;
-        }
-        
+        //translation d'un point 
         public point Transaltion(point q) {
-         point p_resulat = new point (x+q.x , y+q.y);
+            point p_resulat = new point(x + q.x, y + q.y);
             return p_resulat;
-           
-
         }
 
-      public int Distance(point p , point q)
+
+        //calcul de la distance entre deux points
+        public double Distance(point p, point q)
         {
 
-            int resu; 
-            q = new point (x,y);
-            p= new point(x, y);
-            resu = (int)Math.Sqrt( Math.Pow(p.x-q.x,2) - Math.Pow(p.y-q.y,2) ); 
-            return resu; 
+            double resu;
+          
+            resu = Math.Sqrt(Math.Pow(p.x - q.x, 2) - Math.Pow(p.y - q.y, 2));
+            return resu;
 
         }
 
+        // verifie si deux points sont egales 
+        public bool egalite(point p, point q)
+        {
+          
+            return ((p.x == q.x) && (p.y == q.y));
+        }
+
+        // la linearite de 3 points 
+
+        public bool linearite (point p, point q, point k) {
+          
+            double cofi_direc_1 = 0; // initialisation de coefficenient directeur b 0 
+            double cofi_direc_2 = 0;
+            cofi_direc_1 = (p.y - q.y) / (p.x - q.x);
+            cofi_direc_2 = (k.y - p.y) / (k.x - p.x);
+            return (cofi_direc_1 == cofi_direc_2);
+
+
+
+        }
     }
 }
